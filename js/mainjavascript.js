8f84fc86e3f4940fde3fcb300cbfc9586ee3b7b8
simone menichetti <simon3menichettui95@gmail.com>
2024-06-07 16:58:07 +0200

    feat: add js array list autorize

--- js/mainjavascript.js
+++ js/mainjavascript.js
@@ -1,2 +1,20 @@
 // Mail :email:
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+// 1* //  Creazione Array contenente le email autorizzate
+
+const emailList = ["macgyver@example.com", "johncena@example.com", "chucknorris@example.com"];
+
+// 2* Chiedi all’utente la sua email,
+
+
+
+// controlla che sia nella lista di chi può accedere,
+
+
+
+
+
+// stampa un messaggio appropriato sull’esito del controllo.
