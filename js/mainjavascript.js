242f09191b00e533f565e6d2365ccb3bf15fd53e
simone menichetti <simon3menichettui95@gmail.com>
2024-06-07 17:29:47 +0200

    feat: (User Interact) add js const element & event click

--- js/mainjavascript.js
+++ js/mainjavascript.js
@@ -5,9 +5,30 @@
 
 // 1* //  Creazione Array contenente le email autorizzate
 
-const emailList = ["macgyver@example.com", "johncena@example.com", "chucknorris@example.com"];
+    const emailList = ["macgyver@example.com", "johncena@example.com", "chucknorris@example.com"];
+
+    // controllo in console
+
+    console.log(emailList);
 
 // 2* Chiedi all’utente la sua email,
+
+    // estrapolazione elemento del form
+    
+    const formEmail = document.querySelector(".emailform");
+
+    // estrapolazione elemento input 
+
+    const inviaMail = document.querySelector(".submit");
+
+    // aggiunta eventlistener per l'interazione dell'utente al click
+
+    inviaMail.addEventListener ("click",
+        function(event) {
+            event.preventDefault()
+        }
+    )
+
 
 
 
