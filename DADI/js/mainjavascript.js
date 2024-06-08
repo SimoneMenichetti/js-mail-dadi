d0117d1c145237c2b7007728e1918a6424841cef
simone menichetti <simon3menichettui95@gmail.com>
2024-06-08 18:19:03 +0200

    feat(add function play and stamp score)

--- DADI/js/mainjavascript.js
+++ DADI/js/mainjavascript.js
@@ -1,7 +1,7 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto
 
 
-// Generare un numero random da 1 a 6,utilizzando mathflor
+
 
     // creazione const estrapolazione element dal dom
         const bottonePlay = document.getElementById("playButton");
@@ -9,8 +9,25 @@
         bottonePlay.addEventListener("click", play);
 
         function play(){
+             // Generiamo un numero random da 1 a 6 per il giocatore
+             let playerUtente = Math.floor(Math.random() * 6) + 1;
+                // verifichiamo in console numero giocatore utente
+                console.log(playerUtente); 
 
-            
+             // Generiamo un numero random da 1 a 6 per il computer
+             let playerComputer = Math.floor(Math.random() * 6) + 1;
+                 // verifichiamo in console numero computer giocante
+                 console.log(playerComputer); 
+
+
+                  // Mostra i punteggi dell'utente e del computer
+            let risultatoGioco = document.querySelector(".risultato");
+
+            risultatoGioco.innerHTML = "Il giocatore ha ottenuto " + playerUtente + ". Il computer ha ottenuto " +  playerComputer + ".";
+
+
+            // tabilire il vincitore, in base a chi fa il punteggio più alto
+
         }
 
   
