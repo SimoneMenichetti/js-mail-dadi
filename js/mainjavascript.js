5e02e9a1a9eea7590469e8244e3b9243a10ecb28
simone menichetti <simon3menichettui95@gmail.com>
2024-06-08 17:41:34 +0200

    fix(js cicle search email list

--- js/mainjavascript.js
+++ js/mainjavascript.js
@@ -6,11 +6,11 @@
 
 // 1* //  Creazione Array contenente le email autorizzate
 
-    const emailList = ["macgyver@example.com", "johncena@example.com", "chucknorris@example.com"];
+    const emailAutorizzate = ["macgyver@example.com", "johncena@example.com", "chucknorris@example.com"];
 
     // controllo in console
 
-    console.log(emailList);
+    console.log(emailAutorizzate);
 
 // 2* Chiedi all’utente la sua email,
 
@@ -22,6 +22,8 @@
 
     const inviaMail = document.querySelector(".submit");
 
+    const messaggio= document.querySelector(".messaggio");
+
     // aggiunta eventlistener per l'interazione dell'utente al click
 
     inviaMail.addEventListener ("click",
@@ -29,33 +31,42 @@
             event.preventDefault();
 
             const emailInput = document.getElementById('emailinput').value;
+            let emailTrovata = false;
 
-            const testoMessaggio = document.querySelector('.messaggio');
-            
-
-             // verifica funzionamento console
+             // verifica funzionamento console click
     
             console.log(emailInput);
-        }
 
+             // Controlla se l'email è nella lista delle email autorizzate usando un ciclo for
+             for (let i = 0; i < emailAutorizzate.length; i++) {
+                if (emailAutorizzate[i] === emailInput ) {
+                    emailTrovata = true;
+                    
+                    
+                }
+            }
         
+            // Mostra il messaggio appropriato
+            if (emailTrovata) {
+
+                
+                document.querySelector('.messaggio').textContent = "Accesso consentito! sei il benvenuto!";
+                messaggio.style.color= "green";
+                console.log(emailTrovata);
+            } else {
+                document.querySelector('.messaggio').textContent = "Accesso negato! ingresso non autorizzato!";
+                messaggio.style.color= "red";
+                console.log(emailTrovata);
+                
+            }
+
+        }
     )
 
-// controlla che sia nella lista di chi può accedere, iniziando un ciclo con for nella emaillist
-    let accessoConsentito = false;  
 
-    for (let i = 0; i < emailList.length && !accessoConsentito; i++) {
+     
 
-        
-
-        if (emailList[i] === emailInput) {
-
-            accessoConsentito = true;
-            
-            
-       }
-        
-    }
+   
 
 
 
