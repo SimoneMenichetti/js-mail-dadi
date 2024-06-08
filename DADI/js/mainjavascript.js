9b71917c740f63c05faa2b7114cc1b0180e47b16
simone menichetti <simon3menichettui95@gmail.com>
2024-06-08 18:07:57 +0200

    feat(User interation event click)

---
+++ DADI/js/mainjavascript.js
@@ -0,0 +1,16 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto
+
+
+// Generare un numero random da 1 a 6,utilizzando mathflor
+
+    // creazione const estrapolazione element dal dom
+        const bottonePlay = document.getElementById("playButton");
+
+        bottonePlay.addEventListener("click", play);
+
+        function play(){
+
+            
+        }
+
+  
